e777ddc545e5bfe1fb175ffd5a67e830cfc44491
linnabraham <linn.official@gmail.com>
2023-06-02 14:21:14 +0530

    rewrite script for evaluation to be tf 2.0 compatible and other changes

--- evaluate_alexnet.py
+++ evaluate_alexnet.py
@@ -1,93 +1,116 @@
-import os
-from keras.models import load_model, model_from_json
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.utils import to_categorical
-from PIL import Image
-import numpy as np
-
-def read_images(image_paths):
-    images = []
-    for path in image_paths:
-
-        image = Image.open(path)
-        images.append(image)
-    return images
-
 
 if __name__=="__main__":
 
     from alexnet_utils.params import parser
-    parser.add_argument('-model_path',  help="path containing pre-trained model")
-    parser.add_argument('-inputs',  help="path of image or csv file for prediction")
+    parser.add_argument('-model_path',  help="Path of pre-trained model")
+    parser.add_argument('-test_dir',  help="Directory containing validation or test images sorted into respective classes")
     args = parser.parse_args()
 
+    model_path = args.model_path
+    data_dir = args.test_dir
+    target_size = args.target_size
+    img_height, img_width = target_size
+    batch_size = args.batch_size
+    img_height, img_width = target_size
+
+
     # Load the pre-trained model
-    model = load_model(args.model_path)
-    config = model.get_config() # Returns pretty much every information about your model
+    model = load_model(model_path)
+    # Return pretty much every information about your model
+    config = model.get_config() 
+
+    # Return a tuple of width, height and channels as the expected input shape
     print("Expected input shape for the model")
-    print(config["layers"][0]["config"]["batch_input_shape"]) # returns a tuple of width, height and channels
+    print(config["layers"][0]["config"]["batch_input_shape"]) 
 
-    dataset_dir = 'data/train_data/base/test/'
-
-    image_paths = []
-    labels = []
-
-    # Iterate over the subfolders
-    for class_name in os.listdir(dataset_dir):
-        print(class_name)
-        class_dir = os.path.join(dataset_dir, class_name)
-        print(class_dir)
-        if os.path.isdir(class_dir):
-            # Get the class label from the folder name
-            label = class_name  
-            
-            # Iterate over the image files in the subfolder
-            for filename in os.listdir(class_dir):
-                if filename.endswith('.jpeg'):
-                    image_path = os.path.join(class_dir, filename)
-                    image_paths.append(image_path)
-                    labels.append(label)
-
-
-    # Encode labels as 1s and 0s
-    encoded_labels = [1 if label == 'Rings' else 0 for label in labels]
-    ground_truth_labels = encoded_labels
+    test_ds = tf.keras.utils.image_dataset_from_directory(
+      data_dir,
+      #color_mode='grayscale',
+      shuffle=False,
+      seed=123,
+      image_size=(img_height, img_width),
+      batch_size=batch_size)
  
-    images = read_images(image_paths)
-    images_array = np.stack(images)
-    print(images_array.shape)
+    normalization_layer = layers.Rescaling(1./255)
 
-    rescaled_imgs = images_array / 255.0
-    predictions = model.predict(rescaled_imgs)
-    print(predictions)
+    def change_inputs(images, labels, paths):
+      x = normalization_layer(images)
+      return x, labels,  tf.constant(paths)
 
-    # save prediction probabilities to disk
-    filenames = images_array.reshape(-1,1)
-    stack = np.hstack([filenames, predictions])
-    header = "filenames, " + ", ".join(["NonRings","Rings"])
-    savepath =f"output/probabilities_{os.getpid()}.csv"
-    np.savetxt(savepath,stack,delimiter=',',fmt='%s',header = header)
+    normalized_ds = test_ds.map(lambda images, labels: change_inputs(images, labels, paths=test_ds.file_paths))
+    AUTOTUNE = tf.data.AUTOTUNE
+    normalized_ds = normalized_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-    # Convert the predicted probabilities to class labels
+    # create list to store the labels
+    ground_truth = []
+
+    for image_batch, labels_batch, paths in normalized_ds:
+        #predictions = model.predict_on_batch(image_batch)
+        flat = labels_batch.numpy().flatten()
+        ground_truth.extend(flat)
+
+    images, labels, paths = next(iter(normalized_ds.take(1)))
+    filenames = [ path.numpy().decode('utf-8') for path in paths]
+
+    predictions = model.predict(normalized_ds)
+    threshold = 0.5
+    print("Using a classification threshold", threshold)
+
+    #predicted_labels = [1 if pred >= threshold else 0 for pred in predictions[:,-1]] 
     predicted_labels = np.argmax(predictions, axis=1)
 
     # Compute the confusion matrix
     from sklearn.metrics import confusion_matrix
 
-    confusion_mtx = confusion_matrix(ground_truth_labels, predicted_labels)
+    confusion_mtx = confusion_matrix(ground_truth, predicted_labels)
     print("Confusion Matrix:")
     print(confusion_mtx)
 
     # Compute other accuracy metrics
-    from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+    from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, roc_curve
 
-    accuracy = accuracy_score(ground_truth_labels, predicted_labels)
-    precision = precision_score(ground_truth_labels, predicted_labels)
-    recall = recall_score(ground_truth_labels, predicted_labels)
-    f1 = f1_score(ground_truth_labels, predicted_labels)
+    accuracy = accuracy_score(ground_truth, predicted_labels)
+    precision = precision_score(ground_truth, predicted_labels)
+    recall = recall_score(ground_truth, predicted_labels)
+    f1 = f1_score(ground_truth, predicted_labels)
+    roc_auc = roc_auc_score(ground_truth, predictions[:,-1])
 
     print("Accuracy:", accuracy)
     print("Precision:", precision)
     print("Recall:", recall)
     print("F1-score:", f1)
+    print("ROC AUC Score:", roc_auc)
+
+    import csv
+
+    # Example label dictionary for decoding
+    label_dict = {0: "NonRings", 1: "Rings"}
+
+    # Decode predicted labels from indices to text labels
+    decoded_labels = [label_dict[np.argmax(pred)] for pred in predictions]
+
+    # Combine filename, second column of numpy array, and predicted labels
+    rows = [[filename, pred[1], label] for filename, pred, label in zip(filenames, predictions, decoded_labels)]
+
+    # Save rows to a CSV file
+    csv_filename = "eval_output.csv"
+
+    with open(csv_filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Filename", "Prediction", "Label"])  # Write header row
+        writer.writerows(rows)
+
+    false_pos_rate, true_pos_rate, proba = roc_curve(ground_truth, predictions[:, -1])
+    optimal_proba_cutoff = sorted(list(zip(np.abs(true_pos_rate - false_pos_rate), proba)), key=lambda i: i[0], reverse=True)[0][1]
+    print("Optimal probability cutoff", optimal_proba_cutoff)
+
+    roc_predictions = [1 if i >= optimal_proba_cutoff else 0 for i in predictions[:, -1]]
+
+    #print("Accuracy Score Before and After Thresholding:  {}".format(accuracy_score(y_test, predictions), accuracy_score(y_test, roc_predictions)))
+    print("Precision Score After Thresholding: {}".format( precision_score(ground_truth, roc_predictions)))
+    print("Recall Score After Thresholding: {}".format(recall_score(ground_truth, roc_predictions)))
+    print("F1 Score After Thresholding: {}".format( f1_score(ground_truth, roc_predictions)))

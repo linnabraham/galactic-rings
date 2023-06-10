1a3911801da6a3c83fbba7199440d143e291a69e
linnabraham <linn.official@gmail.com>
2023-06-10 18:14:39 +0530

    Add more augmentations during training; clean up code

--- train_alexnet.py
+++ train_alexnet.py
@@ -1,39 +1,74 @@
 import os
-import time
+from time import time
 import json
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-from tensorflow.keras import layers
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
+from tensorflow.keras.callbacks import ModelCheckpoint, Callback, TensorBoard
 from alexnet_utils.params import parser, print_arguments
 from alexnet_utils.alexnet import AlexNet
+
+global random_state
 
 class SaveHistoryCallback(Callback):
     def __init__(self, file_path):
         super().__init__()
         self.file_path = file_path
-        self.history = {'loss': [], 'accuracy': [], 'val_loss': [], 'val_accuracy': []}
+        self.history = {'loss': [], 'val_loss': [], 'auc_pr':[], 'val_auc_pr':[]}
 
     def on_epoch_end(self, epoch, logs=None):
         self.history['loss'].append(logs.get('loss'))
-        self.history['accuracy'].append(logs.get('accuracy'))
         self.history['val_loss'].append(logs.get('val_loss'))
-        self.history['val_accuracy'].append(logs.get('val_accuracy'))
+        self.history['auc_pr'].append(logs.get('auc_pr'))
+        self.history['val_auc_pr'].append(logs.get('val_auc_pr'))
 
         with open(self.file_path, 'w') as f:
             json.dump(self.history, f)
 
-
-# create an output directory to hold saved model, training graphs etc.
-def make_output_dir(output):
+def get_output_dir(output):
     # create a separate output directory for each run
     pid = os.getpid()
     outdir = os.path.join(output,f"{pid}")
-    print(f"Outputs are saved to {outdir}")
-    os.makedirs(outdir)
+
     return pid, outdir
+
+def random_choice(x, size, seed, axis=0, unique=True):
+    dim_x = tf.cast(tf.shape(x)[axis], tf.int64)
+    indices = tf.range(0, dim_x, dtype=tf.int64)
+    sample_index = tf.random.experimental.stateless_shuffle(indices,seed=seed)[:size]
+    sample = tf.gather(x, sample_index, axis=axis)
+
+    return sample, sample_index
+
+def random_int_rot_img(inputs,seed):
+    angles = tf.constant([1, 2, 3, 4])
+    # Make a new seed.
+    new_seed = tf.random.experimental.stateless_split((seed,seed), num=1)[0, :]
+    angle = random_choice(angles,1,seed=new_seed)[0][0]
+    inputs = tf.image.rot90(inputs, k=angle)
+
+    return inputs
+
+def rescale(image, label):
+    image = tf.cast(image, tf.float32)
+    image = (image / 255.0)
+
+    return image, label
+
+# define custom augmentations
+def augment_custom(images, labels):
+    seed=random_state
+    images, labels = rescale(images, labels)
+    # Make a new seed.
+    new_seed = tf.random.experimental.stateless_split((seed,seed), num=1)[0, :]
+    images = tf.image.stateless_random_flip_left_right(images, seed=new_seed)
+    images = tf.image.stateless_random_flip_up_down(images, seed=new_seed)
+    images = tf.image.stateless_random_brightness(images, max_delta=0.2, seed=new_seed)
+    images = tf.image.stateless_random_contrast(images, lower=0.2, upper=0.5, seed=new_seed)
+    images = random_int_rot_img(images,seed=seed)
+
+    return (images, labels)
 
 if __name__=="__main__":
 
@@ -62,56 +97,70 @@ if __name__=="__main__":
       color_mode='rgb',
       seed=random_state,
       image_size=target_size,
-      batch_size=batch_size)
+      batch_size=None)
 
     class_names = train_ds.class_names
-
     print("Training dataset class names are :",class_names)
+    
+    # set the num_classes automatically from the number of directories found  by the data generator
+    #num_classes = len(class_names)
 
-    pid, outdir = make_output_dir(output)
+
+    # create an output directory to hold saved model, training graphs etc.
+    pid, outdir = get_output_dir(output)
+    print(f"Outputs are saved to {outdir}")
+    os.makedirs(outdir)
+
+    # define callbacks
+    tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+    mc = ModelCheckpoint('best_model.h5', monitor='val_loss', \
+            mode='min', verbose=1, save_best_only=True)
+    history_path = os.path.join(outdir,'history.json')
+    hc = SaveHistoryCallback(history_path)
 
     print(f"Saving filenames used for training and validation to disk...")
 
-    filenames = train_ds.file_paths
-    results=pd.DataFrame({"Filename":filenames})
-    results.to_csv(os.path.join(outdir,f"{pid}_train_filenames.csv"),index=False)
+    train_filenames = train_ds.file_paths
+    val_filenames = val_ds.file_paths
 
-    filenames = val_ds.file_paths
-    results=pd.DataFrame({"Filename":filenames})
-    results.to_csv(os.path.join(outdir,f"{pid}_validation_filenames.csv"),index=False)
- 
+    pd.DataFrame({"Filename":train_filenames}).to_csv(os.path.join(outdir,f"{pid}_train_filenames.csv"),index=False)
+    pd.DataFrame({"Filename":val_filenames}).to_csv(os.path.join(outdir,f"{pid}_validation_filenames.csv"),index=False)
 
-    # set the num_classes automatically from the number of directories found 
-    # by the data generator
-    #num_classes = len(class_names)
 
-    normalization_layer = layers.Rescaling(1./255)
-    train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-
-    val_ds = val_ds.map(lambda x, y: (normalization_layer(x), y))
     AUTOTUNE = tf.data.AUTOTUNE
 
-    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    train_ds = (
+            train_ds
+            .shuffle(1000)
+            .map(augment_custom, num_parallel_calls=AUTOTUNE)
+            .cache()
+            .batch(batch_size)
+            .prefetch(buffer_size=AUTOTUNE)
+            )
 
-    # define callbacks
-    mc = ModelCheckpoint('best_model.h5', monitor='val_loss', \
-            mode='min', verbose=1, save_best_only=True)
+    val_ds = (
+            val_ds
+            .map(rescale, num_parallel_calls=AUTOTUNE)
+            .cache()
+            .batch(batch_size)
+            .prefetch(buffer_size=AUTOTUNE)
+            )
 
-    es = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode='auto')
+    # save filenames also along with images and labels in the saved dataset
+    def change_inputs(images, labels, paths):
+      return images, labels,  tf.constant(paths)
 
-    history_path = os.path.join(outdir,'history.json')
-    history_callback = SaveHistoryCallback(history_path)
+    # save validation data for future evaluation
+    val_ds_todisk = val_ds.map(lambda images, labels: change_inputs(images, labels, paths=val_filenames))
 
-    classification_threshold = 0.5
+    path = os.path.join(outdir,"val_data")
+    tf.data.Dataset.save(val_ds_todisk, path)
 
-    METRICS = [
-          tf.keras.metrics.Precision(thresholds=classification_threshold,
-                                     name='precision'),
-          tf.keras.metrics.Recall(thresholds=classification_threshold,
-                                  name="recall"),
-          tf.keras.metrics.AUC(num_thresholds=100, curve='PR', name='auc_pr'),
-    ]
+    # calculate an intial bias to apply based on the class imbalance in the training data
+    pos = train_ds.map(lambda _, label: tf.reduce_sum(label)).reduce(0, lambda count, val: count + val).numpy()
+    neg = train_ds.map(lambda _, label: tf.reduce_sum(1 - label)).reduce(0, lambda count, val: count + val).numpy()
+    initial_bias = np.log([pos/neg])
+    print("[INFO] Calculated initial weight bias:", initial_bias)
 
     existing_modelpath = 'best_model.h5'
 
@@ -119,19 +168,24 @@ if __name__=="__main__":
         print("[INFO] Loading existing model from disk ..")
         model = load_model(existing_modelpath)
     else:
+        classification_threshold = 0.5
 
-        opt = Adam(learning_rate=1e-3)
+        METRICS = [
+              tf.keras.metrics.Precision(thresholds=classification_threshold,
+                                         name='precision'),
+              tf.keras.metrics.Recall(thresholds=classification_threshold,
+                                      name="recall"),
+              tf.keras.metrics.AUC(num_thresholds=100, curve='PR', name='auc_pr'),
+        ]
+
         model = AlexNet.build(width=target_size[0], height=target_size[1], depth=channels, classes=1, reg=0.0002)
 
 
         print("[INFO] compiling model...")
-        model.compile(loss="binary_crossentropy", optimizer=opt, metrics=METRICS)
+        model.compile(loss="binary_crossentropy", optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), metrics=METRICS)
 
-    start = time.time()
+    start = time()
 
-    history = model.fit(train_ds, validation_data=val_ds, epochs=epochs, \
-            callbacks=[mc,
-            #es
-            history_callback])
+    history = model.fit(train_ds, validation_data=val_ds, epochs=epochs, shuffle=True, callbacks=[mc, hc, tensorboard])
 
-    print("Total time taken for training: %d seconds" % (time.time()-start))
+    print("Total time taken for training: %d seconds" % (time()-start))

5aadf29627345e9deb1ba6f4151a040253b824dc
linnabraham <linn.official@gmail.com>
2023-09-22 22:06:17 +0530

    Removed code that is not needed

--- helpers/plot_prcurve.py
+++ helpers/plot_prcurve.py
@@ -15,8 +15,7 @@ if __name__=="__main__":
     prob_colname = sys.argv[3]
     df = pd.read_csv(filename)
     y_test = df[gt_colname]
-    predictions = df[prob_colname]
-    probabilities = predictions.apply(lambda x: float(x.strip('[]')))
+    probabilities = df[prob_colname]
     precision, recall, _ = precision_recall_curve(y_test, probabilities)
     disp = PrecisionRecallDisplay(precision=precision, recall=recall)
     disp.plot()

f865d1606840338655fc21a919832dafea077bf3
linnabraham <linn.official@gmail.com>
2024-07-13 15:51:13 +0530

    Plot histograms for data after applying selection crteria

--- plot_hists.py
+++ plot_hists.py
@@ -12,10 +12,11 @@ def make_subplots(df1, df2):
     df1: buta's data
     df2: nair abraham data
     """
-    ndf = df2
     plt.figure(figsize=(12,6))
     plt.subplot(1, 2, 1)
-    plt.hist(df['z'], bins=50, histtype='step', edgecolor='black', density=True)
+    #plt.hist(df1['z'], bins=50, histtype='step', edgecolor='black', density=True, label='Rings')
+    plt.hist(df1['z'], bins=50, histtype='step', edgecolor='black', weights=np.ones_like(df1['z'])/len(df1['z']), density=False, label='Rings')
+    plt.legend()
     # plt.xlabel('Redshift')
     # plt.ylabel('Counts')
     # plt.ylim(0,900)
@@ -26,7 +27,9 @@ def make_subplots(df1, df2):
     # plt.close()
 
     plt.subplot(1, 2, 2)  # Right subplot
-    plt.hist(ndf['zs'], bins=50, histtype='step', edgecolor='black', density=True)
+    #plt.hist(df2['zs'], bins=50, histtype='step', edgecolor='black', density=True, label='Non-Rings')
+    plt.hist(df2['zs'], bins=50, histtype='step', edgecolor='black', weights=np.ones_like(df2['zs'])/len(df2['zs']), density=False, label='Non-Rings')
+    plt.legend()
     # plt.xlabel('Redshift')
     # plt.ylabel('Counts')
     plt.subplot(1, 2, 2).get_shared_x_axes().join(plt.subplot(1, 2, 1), plt.subplot(1, 2, 2))
@@ -55,11 +58,14 @@ def make_subplots_mag(df1, df2):
     # plt.close()
     # def subpl_1r2c(
     # fig, ax = plt.subplots(1, 2, figsize=(10, 10), sharex=True, sharey=True)
-    g_mag = buta_mag['gmag']-buta_mag['e_gmag']
+    g_mag = df1['gmag'] - df1['e_gmag']
 
     plt.figure(figsize=(12,6))
     plt.subplot(1, 2, 1)
-    plt.hist(ndf['g_mag'], range=(12,16), bins=30, histtype='step', edgecolor='black', density=True)# the extinction corrected g_mag column
+    #plt.hist(g_mag, range=(12,18), bins=30, histtype='step', edgecolor='black', density=True)# the extinction corrected g_mag column
+    #plt.hist(g_mag, bins=30, histtype='step', edgecolor='black', density=True, label='Rings')# the extinction corrected g_mag column
+    plt.hist(g_mag, bins=30, histtype='step', edgecolor='black', weights=np.ones_like(g_mag)/len(g_mag), density=False, label='Rings')
+    plt.legend()
     # plt.xlabel('Extinction corrected g-mag')
     # plt.ylabel('Counts')
     #plt.savefig('na_gmag_dist_python.png')
@@ -67,7 +73,10 @@ def make_subplots_mag(df1, df2):
     #plt.close()
 
     plt.subplot(1, 2, 2)
-    plt.hist(g_mag, range=(12,18), bins=30, histtype='step', edgecolor='black', density=True)# the extinction corrected g_mag column
+    #plt.hist(df2['g_mag'], range=(12,16), bins=30, histtype='step', edgecolor='black', density=True, label='Non-Rings')# the extinction corrected g_mag column
+    #plt.hist(df2['g_mag'], bins=30, histtype='step', edgecolor='black', density=True, label='Non-Rings')# the extinction corrected g_mag column
+    plt.hist(df2['g_mag'], bins=30, histtype='step', edgecolor='black', weights=np.ones_like(df2['g_mag'])/len(df2['g_mag']), density=False, label='Non-Rings')
+    plt.legend()
     # plt.hist(g_mag, bins=30)# the extinction corrected g_mag column
     # plt.xlabel('Extinction corrected g-mag')
     # plt.ylabel('Counts')
@@ -89,9 +98,15 @@ df['z'][df['z']==0.0]=np.nan
 hdul = fits.open("data/catalogs/nair_abraham_2010_vizier.fit")
 ndf = Table(hdul[1].data).to_pandas()
 
-make_subplots(df, ndf)
-
 buta_mag = pd.read_csv("data/catalogs/buta_with_mag.csv")
+buta_mag = buta_mag[(buta_mag["gmag"] - buta_mag["e_gmag"]) < 16]
+buta_mag = buta_mag[buta_mag.z != '---']
+buta_mag.z = buta_mag.z.astype(float)
+buta_mag = buta_mag[~(buta_mag.z < 0.01) & ~(buta_mag.z > 0.1) ]
 
+make_subplots(buta_mag, ndf)
+print(min(buta_mag.gmag), max(buta_mag.gmag))
+print(min(ndf.g_mag), max(ndf.g_mag))
+ndf = ndf[~(ndf.g_mag == 0)]
 make_subplots_mag(buta_mag, ndf)
 
